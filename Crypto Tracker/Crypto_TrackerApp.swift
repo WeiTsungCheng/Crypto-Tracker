b3aa47d1d4fec729f83123326365ca2429dace40
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-03-28 14:53:06 +0000

    Initial Commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by WEITSUNG on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
