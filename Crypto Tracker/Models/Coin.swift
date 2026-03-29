41adec888206dbee8fee6a0b3a2c27f480d12b40
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-03-29 17:30:39 +0100

    update Coin model

--- Crypto Tracker/Models/Coin.swift
+++ Crypto Tracker/Models/Coin.swift
@@ -5,16 +5,26 @@
 //  Created by WEITSUNG on 28/03/2026.
 //
 
-struct Coin: Identifiable, Codable {
+import Foundation
+
+struct Coin: Codable, Identifiable {
     let id: String
     let name: String
     let symbol: String
     let currentPrice: Double
-    
+    let marketCapRank: Int?
+    let high24h: Double?
+    let low24h: Double?
+    let priceChangePercentage24h: Double?
+
     enum CodingKeys: String, CodingKey {
         case id
         case name
         case symbol
         case currentPrice = "current_price"
+        case marketCapRank = "market_cap_rank"
+        case high24h = "high_24h"
+        case low24h = "low_24h"
+        case priceChangePercentage24h = "price_change_percentage_24h"
     }
 }
